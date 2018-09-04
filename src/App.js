c4acb3e9a262a3c39e01b99e03dfa08db673c943
arthur <ganievart@gmail.com>
2018-09-05 00:05:28 +0200

    Add header footer homePage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
+import './Assets/css/default.min.css'
+
+import Header from './components/headerComponent/header'
+import Footer from './components/footerComponent/footer'
+import HomePage from './components/pages/homePage.js'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        TEST TEST
+
+        <Header />
+
+          <HomePage />
+
+        <Footer />
+
       </div>
     );
   }
